d9d2a92dbf4dc04f43ca6e1af78576b0efc52454
dipu <dipu.j247@gmail.com>
2021-06-23 19:14:50 +0600

    data types

---
+++ second_cls_23_06_21/data_type.py
@@ -0,0 +1,25 @@
+# premitive, non premitive
+
+# premitive -> integer, float, boolean, char
+# non premitive -> string, array
+
+
+# python
+# int, float, string, boolean
+
+import numpy as np
+
+integer = 10
+float = 10.45
+
+string = "This a django course"
+char = 'a'
+
+boolean = True
+boolean_ = False
+
+arr = np.array([1, 2, 3, 4, 5])
+
+print(integer, float, string, char, boolean_, boolean)
+print(type(integer), type(float), type(string), type(char), type(boolean_), type(boolean))
+print(arr)
